b0b9bc3c04222fb89e394b88125a143545480202
zssAna <ana29chialda@gmail.com>
2025-10-29 01:05:12 +0200

    AirplaneService

---
+++ src/main/java/com/example/flight/demo/service/AirplaneService.java
@@ -0,0 +1,24 @@
+package com.example.flight.demo.service;
+
+import com.example.flight.demo.model.Airplane;
+import com.example.flight.demo.repository.AirplaneRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AirplaneService {
+    private final AirplaneRepository airplanes;
+
+    public AirplaneService(AirplaneRepository airplanes) {
+        this.airplanes = airplanes;
+    }
+
+    public Airplane save(Airplane a) { return airplanes.save(a); }
+    public boolean delete(Long id) { return airplanes.delete(id); }
+    public Optional<Airplane> find(Long id) { return airplanes.findById(id); }
+    public List<Airplane> all() { return airplanes.findAll(); }
+
+    public Optional<Airplane> findByNumber(int number) { return airplanes.findByNumber(number); }
+}
