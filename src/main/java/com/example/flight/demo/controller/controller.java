d6dd6e72823afccb926170070315c1edde3299a4
Andreea Badiu <andreea.badiu@stud.ubbcluj.ro>
2025-10-29 02:10:12 +0200

    Controller

--- src/main/java/com/example/flight/demo/controller/controller.java
+++ src/main/java/com/example/flight/demo/controller/controller.java
@@ -1,5 +1,15 @@
 package com.example.flight.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class controller {
-    //clas
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Flight Management System is running!";
+    }
 }
